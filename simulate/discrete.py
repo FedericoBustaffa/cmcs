e8199c42a4806a9cba5a0bfd55eb758595fa50f2
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-09-15 19:24:22 +0200

    discrete simulations

--- simulate/discrete.py
+++ simulate/discrete.py
@@ -1,24 +1,40 @@
 import matplotlib.pyplot as plt
 
 
-class Simulator:
-    def __init__(self, func, *params):
-        self.update = func
-        self.params = params
+def plot_simulation(times: list[int], results: list):
+    plt.figure(figsize=(6, 4), dpi=200)
+    for r in results:
+        plt.plot(
+            times,
+            r,
+            marker="o",
+            mfc="none",
+        )
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
 
-    def run(self, initial_values, steps):
-        values = [initial_values]
-        for _ in range(steps):
-            values.append(self.update(initial_values, *self.params))
-            initial_values = values[-1]
 
-        return [list(i) for i in list(zip(*values))]
+def discrete_simulation(
+    func, initial_values: list, parameters: list, timesteps: int, step: int = 1, start=0
+):
+    values = [initial_values]
+    times = [t for t in range(start, timesteps + 1, step)]
+    for _ in times[1:]:
+        values.append(func(initial_values, parameters))
+        initial_values = values[-1]
+
+    results = [list(i) for i in list(zip(*values))]
+    plot_simulation(times, results)
+
+    return results
 
 
 if __name__ == "__main__":
 
-    def density(n0, alpha, beta, gamma):
+    def density(n0, params):
         a0, c0 = n0
+        alpha, beta, gamma = params
         a_new = (1 - alpha) * a0 + gamma * c0
         c_new = (1 - gamma) * c0 + beta * a0
         return a_new, c_new
@@ -26,21 +42,10 @@ if __name__ == "__main__":
     a0 = 50  # initial adults
     c0 = 50  # initial children
     alpha = 0.01  # adults death rate
-    beta = 0.05  # children birth rate
+    beta = 0.01  # children birth rate
     gamma = 0.18  # children to adults rate
-    years = 50
+    years = 100
 
-    sim = Simulator(density, alpha, beta, gamma)
-    adults, children = sim.run((a0, c0), years)
-    times = [t for t in range(years + 1)]
-
-    plt.figure(dpi=200)
-    plt.title(
-        f"Adults and Children Density\n $a_0 = {a0}, c_0 = {c0}, \\alpha = {alpha}, \\beta={beta}, \\gamma = {gamma}$"
+    adults, children = discrete_simulation(
+        density, [a0, c0], [alpha, beta, gamma], years, 3
     )
-    plt.plot(times, adults, label="adults")
-    plt.plot(times, children, label="children")
-    plt.plot(times, [a + c for a, c in zip(adults, children)], label="total")
-    plt.legend()
-    plt.grid()
-    plt.show()
