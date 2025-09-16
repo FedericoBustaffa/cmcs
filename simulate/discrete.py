d6d27fa79b255f440086bd09dc245f43b9841cdc
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-09-16 23:23:50 +0200

    discrete

--- simulate/discrete.py
+++ simulate/discrete.py
@@ -1,18 +1,21 @@
-def discrete_simulation(
-    func,
-    state: list,
-    parameters: list,
-    timesteps: int,
-    step: int = 1,
-    start=0,
-):
-    values = [state]
-    times = [t for t in range(start, timesteps + 1, step)]
-    for _ in times[1:]:
-        values.append(func(state, parameters))
-        state = values[-1]
+from typing import Callable, Sequence
 
-    return [list(i) for i in list(zip(*values))]
+import numpy as np
+
+
+def discrete(
+    func: Callable,
+    state: Sequence[float],
+    params: Sequence[float],
+    steps: int,
+) -> tuple[np.ndarray, np.ndarray]:
+    values = np.zeros(shape=(steps, len(state)))
+    values[0] = np.asarray(state)
+    timesteps = np.arange(steps)
+    for t in timesteps[:-1]:
+        values[t + 1] = func(values[t], params)
+
+    return timesteps, values.T
 
 
 if __name__ == "__main__":
@@ -25,25 +28,27 @@ if __name__ == "__main__":
         c_new = (1 - gamma) * c0 + beta * a0
         return a_new, c_new
 
-    a0 = 50  # initial adults
-    c0 = 50  # initial children
-    alpha = 0.01  # adults death rate
-    beta = 0.01  # children birth rate
-    gamma = 0.18  # children to adults rate
-    years = 100
-    step = 3
+    a0 = 500  # initial adults
+    c0 = 1000  # initial children
+    alpha = 0.05  # adults death rate
+    beta = 0.1  # children birth rate
+    gamma = 1 / 18  # children to adults rate
+    years = 50
 
-    adults, children = discrete_simulation(
-        density, [a0, c0], [alpha, beta, gamma], years, step
+    timesteps, (adults, children) = discrete(
+        density, [a0, c0], [alpha, beta, gamma], years
     )
+    total = adults + children
+
     print(f"adults final density: {adults[-1]:.2f}")
     print(f"children final density: {children[-1]:.2f}")
+    print(f"total density: {total[-1]:.2f}")
 
-    times = [t for t in range(0, years + 1, 3)]
     plt.figure(dpi=150)
     plt.title("Adults and Children Densities")
-    plt.plot(times, adults, marker="o", mfc="none", label="adults")
-    plt.plot(times, children, marker="o", mfc="none", label="children")
+    plt.plot(timesteps, adults, marker="o", mfc="none", label="adults")
+    plt.plot(timesteps, children, marker="o", mfc="none", label="children")
+    plt.plot(timesteps, total, marker="o", mfc="none", label="total")
     plt.grid()
     plt.legend()
     plt.tight_layout()
