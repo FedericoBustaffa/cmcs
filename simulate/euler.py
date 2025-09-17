3a1490fe3e9c5c56eddd9af65f1a93ee478f75f2
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-09-17 23:25:05 +0200

    implicit euler method

--- simulate/euler.py
+++ simulate/euler.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
+from scipy.optimize import fsolve
 
 
 def c_birth(n0, rate, t):
@@ -11,7 +12,7 @@ def density(n, t, rate):
     return rate * n
 
 
-def euler(y0: float, rate: float, start: float, step: float, stop: float):
+def explicit(y0: float, rate: float, start: float, step: float, stop: float):
     timesteps = np.arange(start, stop + step, step)
     results = [y0]
 
@@ -23,31 +24,47 @@ def euler(y0: float, rate: float, start: float, step: float, stop: float):
     return timesteps, np.asarray(results)
 
 
+def implicit(y0: float, rate: float, start: float, step: float, stop: float):
+    timesteps = np.arange(start, stop + step, step)
+    results = [y0]
+
+    y = y0
+    for _ in timesteps[:-1]:
+        func = lambda y1: y1 - y - step * rate * y1
+        y = fsolve(func, y)[0]
+        results.append(y)
+
+    return timesteps, np.asarray(results)
+
+
 if __name__ == "__main__":
     N_0 = 5
     rate = np.log(1.5)
     times = np.linspace(0, 10, 50)
+    step = 0.5
 
     plt.figure(figsize=(8, 5), dpi=200)
-    plt.title("Euler Method")
+    plt.title(rf"Euler Method ($\tau = {step:.1f}$)")
 
     # exact solution
     plt.plot(times, [c_birth(N_0, rate, t) for t in times], label="exact")
 
     # euler
-    for step in [0.2, 1.0]:
-        e_times, e_values = euler(N_0, rate, 0, step, 10)
-        plt.plot(e_times, e_values, marker="s", label=rf"$\tau = {step:.2f}$")
+    i_times, i_values = explicit(N_0, rate, 0, step, 10)
+    plt.plot(i_times, i_values, marker="s", label="explicit")
+
+    i_times, i_values = implicit(N_0, rate, 0, step, 10)
+    plt.plot(i_times, i_values, marker="s", label="implicit")
 
     # scipy odeint
     times = np.linspace(0, 10, 10)
     sol = odeint(density, N_0, times, args=(rate,))
-    plt.plot(times, sol, marker="s", label="scipy")
+    # plt.plot(times, sol, marker="s", label="scipy")
 
     plt.xlabel("Time")
     plt.ylabel("Density")
     plt.grid()
     plt.legend()
     plt.tight_layout()
-    # plt.savefig("/home/federico/obsidian/master/files/euler.png")
+    # plt.savefig("/home/federico/obsidian/master/files/euler2.png")
     plt.show()
