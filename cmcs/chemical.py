3ccb1493a2677f1143bb6d8285c27afcaca05e0a
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-09-23 16:14:12 +0200

    reactions

--- cmcs/chemical.py
+++ cmcs/chemical.py
@@ -1,16 +1,29 @@
 class Reaction:
-    def __init__(self, r, p, k):
-        self.r = r
-        self.p = p
-        self.k = k
+    def __init__(self, r, rc, p, pc, k) -> None:
+        self.reactants = r
+        self.react_coeffs = rc
+        self.products = p
+        self.prod_coeff = pc
+        self.kinetic_constant = k
 
+    def __repr__(self) -> str:
+        formula = ""
+        for i, r in enumerate(self.reactants):
+            char = (
+                f" ->{self.kinetic_constant} "
+                if i == (len(self.reactants) - 1)
+                else " + "
+            )
+            coeff = "" if self.react_coeffs[i] == 1 else str(self.react_coeffs[i])
+            formula += coeff + r + char
 
-def to_ode(r: Reaction):
-    pass
+        for i, p in enumerate(self.products):
+            coeff = "" if self.prod_coeff[i] == 1 else str(self.prod_coeff[i])
+            formula += coeff + p
+
+        return formula
 
 
 if __name__ == "__main__":
-    reactants = {"s": 1, "i": 1}
-    products = {"i": 2}
-    kinetic_constant = 3
-    r = Reaction(reactants, products, kinetic_constant)
+    infection = Reaction(["S", "I"], [1, 1], ["I"], [2], 3)
+    print(infection)
